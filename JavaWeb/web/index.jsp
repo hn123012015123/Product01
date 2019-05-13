a4693f3239ebf2b976d7861f713ecc432529d96f
huangning <1085581172@qq.com>
2019-05-13 16:41:15 +0800

    Initial commit

---
+++ JavaWeb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: huangning
+  Date: 2019/5/13
+  Time: 13:45
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>黄宁测试</title>
+  </head>
+  <body>
+  <h1 style="color: orange">你好</h1>
+  </body>
+</html>
---
+++ src/com/atguigu/java/Helloworld.java
@@ -0,0 +1,82 @@
+package com.atguigu.java;
+//
+//import java.util.ArrayList;
+//
+public class Helloworld {
+//
+//    //prsf
+//    private static final;
+//
+//    //psf
+//    public static final int A;
+//
+//    static {
+//        A = 1;
+//    }
+//    //psfi
+//    public static final int ;
+//
+//    //psfs
+//    public static final String ;
+//
+//    //模板psvm
+   public static void main(String[] args){
+//
+//
+//        System.out.println("helloe World");
+//        //sout
+//        System.out.println();
+//        //fori  for循环
+//        String[] arr = new String[]{"aa","bb"};
+//        for (int i = 0; i <arr.length ; i++) {
+//
+//        }
+//        //iter
+//        for (String s : arr) {
+//
+//        }
+//        //itar
+//        for (int i = 0; i < arr.length; i++) {
+//            String s = arr[i];
+//
+//        }
+//        //list.for
+//        ArrayList list = new ArrayList();
+//        for (Object o : list) {
+//
+//        }
+//        //list.fori
+//        for (int i = 0; i < list.size(); i++) {
+//
+//        }
+//
+//        //list.forr
+//        for (int i = list.size() - 1; i >= 0; i--) {
+//
+//        }
+//        //ifn
+//        if (list == null) {
+//
+//        }
+//        //inn
+//        if (list != null) {
+//
+//        }
+//        //xxx.nn和xxx.null
+//        if (list != null) {
+//
+//        }
+//        if (list == null) {
+//
+//        }
+//        //test
+//        public void testAAA(){
+//
+//        }
+//
+//
+//
+//
+    }
+//
+}
